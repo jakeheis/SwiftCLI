d3cff8d802c6984848be22ed583e8d1b4762f661
Jake Heiser <jake@quizlet.com>
2014-07-31 16:56:07 -0500

    Basic example app

---
+++ Example/Example/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Example
+//
+//  Created by Jake Heiser on 7/31/14.
+//  Copyright (c) 2014 jakeheis. All rights reserved.
+//
+
+import Foundation
+
+CLI.registerChainableCommand(commandName: "eat")
+    .withShortDescription("Eats the given food")
+    .withSignature("<food>")
+    .onExecution({parameters, options in
+        let yummyFood = parameters["food"] as String
+        println("Eating \(yummyFood).")
+        return (true, nil)
+    })
+
+CLI.go()
