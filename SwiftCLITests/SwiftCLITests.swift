0a0a86b990762031aa044f04fadb7fd9e70f6be1
Jake Heiser <jake@quizlet.com>
2015-01-08 09:13:00 -0600

    Integration test, full CLI process

--- SwiftCLITests/SwiftCLITests.swift
+++ SwiftCLITests/SwiftCLITests.swift
@@ -11,14 +11,47 @@ import XCTest
 
 class SwiftCLITests: XCTestCase {
     
+    var silentFlag = false
+    var times: Int = 1
+    var executionString = ""
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        CLI.setup(name: "tester")
+        CLI.registerChainableCommand(commandName: "test")
+            .withSignature("<testName> [<testerName>]")
+            .withFlagsHandled(["-s"], block: {(flag) in
+                self.silentFlag = true
+            }, usage: "Silent flag")
+            .withKeysHandled(["-t"], block: {(key, value) in
+                self.times = value.toInt()!
+            }, usage: "Times to test", valueSignature: "times")
+            .withExecutionBlock {(arguments, options) in
+                let testerName = arguments["testerName"] as String? ?? "Tester"
+                let testName = arguments["testName"] as String
+                self.executionString = "\(testerName) will test \(testName), \(self.times) times"
+                if self.silentFlag {
+                    self.executionString += ", silently"
+                }
+                return .Success
+            }
     }
     
+    // Integration test
+    
+    func testCLIGo() {
+        let result = CLI.debugGoWithArgumentString("tester test firstTest MyTester -t 5 -s")
+        XCTAssertEqual(result, CLIResult.Success, "Command should have succeeded")
+        XCTAssertEqual(self.executionString, "MyTester will test firstTest, 5 times, silently", "Command should have produced accurate output")
+    }
+    
+    // Tear down
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        executionString = ""
     }
     
 }
