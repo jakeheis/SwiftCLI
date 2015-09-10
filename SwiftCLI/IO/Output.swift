4916ac0c692cce79c4b076db2b6e26532c69eb4d
Jake Heiser <jake@quizlet.com>
2015-09-10 15:57:28 -0500

    Updated print signature

--- SwiftCLI/IO/Output.swift
+++ SwiftCLI/IO/Output.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 func printError(error: String) {
-    printError(error, appendNewline: true)
+    printError(error, terminator: "\n")
 }
 
-func printError(error: String, appendNewline: Bool) {
+func printError(error: String, terminator: String) {
     let handle = NSFileHandle.fileHandleWithStandardError()
-    let fullString = error + (appendNewline ? "\n" : "")
+    let fullString = error + terminator
     if let errorData = fullString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
         handle.writeData(errorData)
     } else {
