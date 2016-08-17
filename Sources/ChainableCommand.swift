ac00b534ec45356db4b270792ebba6d76f582553
Jake Heiser <jakeheiser1@gmail.com>
2016-08-16 19:37:04 -0500

    Allow chainable command function returns to be discarded

--- Sources/ChainableCommand.swift
+++ Sources/ChainableCommand.swift
@@ -14,36 +14,43 @@ public class ChainableCommand: LightweightCommand {
         super.init(commandName: commandName)
     }
     
+    @discardableResult
     public func withSignature(_ signature: String) -> ChainableCommand {
         commandSignature = signature
         return self
     }
     
+    @discardableResult
     public func withShortDescription(_ shortDescription: String) -> ChainableCommand {
         commandShortDescription = shortDescription
         return self
     }
     
+    @discardableResult
     public func withShortcut(_ shortcut: String) -> ChainableCommand {
         commandShortcut = shortcut
         return self
     }
     
+    @discardableResult
     public func withOptionsSetup(_ optionsSetup: OptionsSetupBlock) -> ChainableCommand {
         optionsSetupBlock = optionsSetup
         return self
     }
     
+    @discardableResult
     public func withUnrecognizedOptionsPrintingBehavior(_ behavior: UnrecognizedOptionsPrintingBehavior) -> ChainableCommand {
         unrecognizedOptionsPrintingBehavior = behavior
         return self
     }
     
+    @discardableResult
     public func withFailOnUnrecognizedOptions(_ shouldFail: Bool) -> ChainableCommand {
         failOnUnrecognizedOptions = shouldFail
         return self
     }
     
+    @discardableResult
     public func withExecutionBlock(_ execution: ExecutionBlock) -> ChainableCommand {
         executionBlock = execution
         return self
