0931c851237a92e1cdef56e239104fa3943bd088
Jake Heiser <jakeheiser1@gmail.com>
2016-10-22 10:28:19 -0700

    Make all chainable withs discardable

--- Sources/ChainableCommand.swift
+++ Sources/ChainableCommand.swift
@@ -10,31 +10,37 @@
 /// Should only be used for simple commands.
 public class ChainableCommand: LightweightCommand {
     
+    @discardableResult
     public func withSignature(_ signature: String) -> ChainableCommand {
         self.signature = signature
         return self
     }
     
+    @discardableResult
     public func withShortDescription(_ shortDescription: String) -> ChainableCommand {
         self.shortDescription = shortDescription
         return self
     }
     
+    @discardableResult
     public func withOptionsSetup(_ optionsSetup: @escaping OptionsSetupBlock) -> ChainableCommand {
         optionsSetupBlock = optionsSetup
         return self
     }
     
+    @discardableResult
     public func withUnrecognizedOptionsPrintingBehavior(_ behavior: UnrecognizedOptionsPrintingBehavior) -> ChainableCommand {
         unrecognizedOptionsPrintingBehavior = behavior
         return self
     }
     
+    @discardableResult
     public func withFailOnUnrecognizedOptions(_ shouldFail: Bool) -> ChainableCommand {
         failOnUnrecognizedOptions = shouldFail
         return self
     }
     
+    @discardableResult
     public func withExecutionBlock(_ execution: @escaping ExecutionBlock) -> ChainableCommand {
         executionBlock = execution
         return self
