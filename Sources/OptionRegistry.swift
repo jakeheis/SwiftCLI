9ed0e59d0c65f74db413846a8966a51ccdb44668
Gregorio Litenstein <g.litenstein@gmail.com>
2017-01-31 18:08:56 -0300

    Define structure of Option groups
    And make sure options inherit the appropriate properties from their declared group or, “options”, if none was specified. The default “options” group functions in exactly the same way as options in the current release of SwiftCLI.

--- Sources/OptionRegistry.swift
+++ Sources/OptionRegistry.swift
@@ -6,16 +6,66 @@
 //  Copyright (c) 2014 jakeheis. All rights reserved.
 //
 
+internal class OptionGroup: Hashable, Equatable {
+    /// The hash value.
+    ///
+    /// Hash values are not guaranteed to be equal across different executions of
+    /// your program. Do not save hash values to use during a future execution.
+    public var hashValue: Int
+
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    public static func ==(lhs: OptionGroup, rhs: OptionGroup) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+
+    public var name: String
+    public var required: Bool
+    public var conflicting: Bool
+    
+    public init(name: String, required: Bool = false, conflicting: Bool = true) {
+        self.hashValue = name.hashValue
+        self.name = name
+        self.required = required
+        self.conflicting = conflicting
+    }
+    
+
+}
+
 public class OptionRegistry {
     
     public typealias FlagBlock = () -> ()
     public typealias KeyBlock = (_ value: String) -> ()
     
     private(set) public var options: [Option] = []
-    
+    internal var groups: Set<OptionGroup> = [OptionGroup(name:"options",required:false,conflicting:false)]
+
+    internal var requiredOptions: [Option] {
+        var groupedRequiredOptions: [Option] = []
+        var groupedOptionStrings: [String] = []
+        var requiredOptions = options.filter() { $0.required == true }
+
+        for group in (groups.filter() {$0.required == true}) {
+            let groupName: String = group.name
+            let groupOptions = requiredOptions.filter() { $0.group == groupName }
+            groupedOptionStrings = groupOptions.flatMap {
+                (optionValue) -> [String] in
+                let value = optionValue.options
+                return value
+            }
+            groupedRequiredOptions.append(Option(options:groupedOptionStrings,usage:"",required:true,group:groupName))
+        }
+        return groupedRequiredOptions
+    }
     private(set) public var flagBlocks: [String: FlagBlock] = [:]
     private(set) public var keyBlocks: [String: KeyBlock] = [:]
-    
     internal(set) public var exitEarlyOptions: [String] = []
     
     // MARK: - Adding options
@@ -28,9 +78,20 @@ public class OptionRegistry {
         - Parameter usage: the usage of these flags, printed in the command usage statement
         - Parameter block: the block to be called upon recognition of the flags
     */
-    public func add(flags: [String], usage: String = "", block: @escaping FlagBlock) {
+    public func addGroup(name: String, required: Bool = false, conflicting: Bool = true) {
+        precondition(name != "", "Must specify a group name.")
+        precondition(!groups.contains(OptionGroup(name:name)),"Cannot declare a group twice.")
+        groups.insert(OptionGroup(name:name, required:required))
+    }
+    public func add(flags: [String], usage: String = "", group: String = "options", block: @escaping FlagBlock) {
+        var required:Bool
         precondition(!flags.isEmpty, "At least one flag must be added")
-        options.append(Option(options: flags, usage: usage))
+            precondition(groups.contains(OptionGroup(name:group)),"Undefined Group: \(group)")
+            if let temp = (groups.first { $0.name == group }) {
+                required = temp.required
+            }
+            else { required = false }
+        options.append(Option(options: flags, usage: usage, required:required,group:group))
         for flag in flags {
             flagBlocks[flag] = block
         }
@@ -53,9 +114,15 @@ public class OptionRegistry {
                                     takes the form "-m, --myKey <valueSignature>"
         - Parameter block: the block to be called upon recognition of the keys
     */
-    public func add(keys: [String], usage: String = "", valueSignature: String = "value", block: @escaping KeyBlock) {
+    public func add(keys: [String], usage: String = "", valueSignature: String = "value", group: String = "options", block: @escaping KeyBlock) {
         precondition(!keys.isEmpty, "At least one key must be added")
-        options.append(Option(options: keys, usage: usage, preusage: "<\(valueSignature)>"))
+        var required:Bool
+        precondition(groups.contains(OptionGroup(name:group)),"Undefined Group: \(group)")
+            if let temp = (groups.first { $0.name == group }) {
+                required = temp.required
+            }
+            else { required = false }
+        options.append(Option(options: keys, usage: usage, preusage: "<\(valueSignature)>",required:required,group:group))
         for key in keys {
             keyBlocks[key] = block
         }
@@ -82,17 +149,22 @@ public typealias Options = OptionRegistry
 
 public class Option {
     
+    public let required: Bool
+    public let group: String
     public let options: [String]
     public let usage: String
     
-    init(options: [String], usage: String, preusage: String? = nil) {
+    init(options: [String], usage: String, preusage: String? = nil, required: Bool = false, group: String = "options") {
         self.options = options
+        self.required = required
+        self.group = group
         
-        var optionsString = options.joined(separator: ", ")
+        var optionsString = options.joined(separator:", ")
+        
         if let preusage = preusage {
             optionsString += " \(preusage)"
         }
-        let spacing = String(repeating: " ", count: 40 - optionsString.characters.count)
+        let spacing = String(repeating: " ", count: 60 - optionsString.characters.count)
         self.usage = "\(optionsString)\(spacing)\(usage)"
     }
     
