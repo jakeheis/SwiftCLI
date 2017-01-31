897d2f786ba05d4fef525d2a19799a532d81cef6
Gregorio Litenstein <g.litenstein@gmail.com>
2017-01-31 18:15:54 -0300

    Implement logic for OptionGroup attributes.
    “required” define sets of options of which AT LEAST ONE must be present for the syntax to be considered valid.
    
    “conflicting” define sets of options of which AT MOST ONE must be present for the syntax to be considered valid.
    At least for now, “conflicting” is ignored if the group is not also “required”

--- Sources/OptionParser.swift
+++ Sources/OptionParser.swift
@@ -8,6 +8,8 @@
 
 // MARK: - OptionParser
 
+import Foundation
+
 public protocol OptionParser {
     func recognizeOptions(in rawArguments: RawArguments, from optionRegistry: OptionRegistry) -> OptionParserResult
 }
@@ -30,7 +32,55 @@ public class DefaultOptionParser: OptionParser {
         var unrecognizedOptions: [String] = []
         var keysNotGivenValue: [String] = []
         var exitEarly: Bool = false
+        var requiredOptionsMissing: [String:String] = [:]
+        var conflictingOptions: [String:[String]] = [:]
         
+        let requiredGroups = optionRegistry.requiredOptions
+        
+        let optionsString = (optionArguments.flatMap { (option) -> [String] in
+            let value = option.value
+            return [value]
+        }).joined(separator: " ")
+        let optionsRange = NSRange(location: 0, length: optionsString.characters.count)
+        for requiredGroup in requiredGroups {
+            var isCurrentGroupConflicting: Bool = true
+            var groupName: String = ""
+            
+            if let currentGroup = (optionRegistry.groups.first { $0.name == requiredGroup.group }) {
+                isCurrentGroupConflicting = currentGroup.conflicting
+                groupName = currentGroup.name
+            }
+            
+            let value = (requiredGroup.options).joined(separator:"|")
+            let patternString = "((\\s(\(value))\\s)|(^(\(value))\\s)|(\\s(\(value))$))"
+            let requiredRegex = try! Regex(pattern: patternString, options: [.caseInsensitive])
+            
+            if requiredRegex.numberOfMatches(in: optionsString.replacingOccurrences(of: " ", with: "  "), options: [], range: optionsRange) < 1 {
+                requiredOptionsMissing[groupName] = value
+            }
+            
+
+            if (isCurrentGroupConflicting == true) {
+                
+                let matches = requiredRegex.matches(in: optionsString.replacingOccurrences(of: " ", with: "  "), options: [], range: optionsRange)
+                
+                if (matches.count > 1) {
+                    var groupConflicts: [String] = []
+                    
+                    for match in matches {
+                        let range = match.range
+                        
+                        let text =  (optionsString.replacingOccurrences(of: " ", with: "  ").substring(from:range.location,length:((range.length) - 1))).replacingOccurrences(of: " ", with: "")
+                        groupConflicts.append(text)
+                    }
+                    
+                    conflictingOptions[groupName] = groupConflicts
+                    
+                }
+            
+            }
+            
+        }
         for optionArgument in optionArguments {
             optionArgument.classification = .option
             if let flagBlock = optionRegistry.flagBlocks[optionArgument.value] {
@@ -55,8 +105,8 @@ public class DefaultOptionParser: OptionParser {
             return .exitEarly
         }
         
-        if !unrecognizedOptions.isEmpty || !keysNotGivenValue.isEmpty {
-            let incorrect = IncorrectOptionUsage(optionRegistry: optionRegistry, unrecognizedOptions: unrecognizedOptions, keysNotGivenValue: keysNotGivenValue)
+        if !unrecognizedOptions.isEmpty || !keysNotGivenValue.isEmpty || !requiredOptionsMissing.isEmpty || !conflictingOptions.isEmpty {
+            let incorrect = IncorrectOptionUsage(optionRegistry: optionRegistry, unrecognizedOptions: unrecognizedOptions, keysNotGivenValue: keysNotGivenValue, requiredOptionsMissing: requiredOptionsMissing, conflictingOptions:conflictingOptions)
             return .incorrectOptionUsage(incorrect)
         }
         
@@ -70,6 +120,8 @@ public struct IncorrectOptionUsage {
     public let optionRegistry: OptionRegistry
     public let unrecognizedOptions: [String]
     public let keysNotGivenValue: [String]
+    public let requiredOptionsMissing: [String:String]
+    public let conflictingOptions: [String:[String]]
     
     public func misusedOptionsPresent() -> Bool {
         return unrecognizedOptions.count > 0 || keysNotGivenValue.count > 0
@@ -78,6 +130,16 @@ public struct IncorrectOptionUsage {
     public func misusedOptionsMessage() -> String {
         var message = ""
         
+        if requiredOptionsMissing.count > 0 {
+            
+            message += "Missing options:"
+            for option in requiredOptionsMissing.sorted(by:{$0.key < $1.key}) {
+                message += "\n\t\(option.key):\t\(option.value)".replacingOccurrences(of:"|", with:", ", options: [])
+            }
+            
+            message += "\n"
+        }
+        
         if unrecognizedOptions.count > 0 {
             message += "Unrecognized options:"
             for option in unrecognizedOptions {
@@ -85,6 +147,17 @@ public struct IncorrectOptionUsage {
             }
             
             message += "\n"
+        }
+        
+        if conflictingOptions.count > 0 {
+        
+            message+="Conflicting options:"
+            for group in conflictingOptions {
+                let groupKeys = group.value.joined(separator: ", ")
+                message += "\n\t\(group.key):\t\(groupKeys)"
+            
+            }
+        
         }
         
         if keysNotGivenValue.count > 0 {
