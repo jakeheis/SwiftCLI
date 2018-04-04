0f10cb9680f0b6b319dc95e3c1c3e28ccd19c976
Jake Heiser <jakeheiser1@gmail.com>
2018-04-04 16:27:21 -0500

    Docs

--- Sources/SwiftCLI/Task.swift
+++ Sources/SwiftCLI/Task.swift
@@ -9,22 +9,48 @@ import Foundation
 
 // MARK: -
 
+/// Run an executable synchronously; uses this process's stdout, stderr, and stdin
+///
+/// - Parameters:
+///   - executable: the executable to run
+///   - args: arguments to pass to the executable
+/// - Throws: RunError if command fails
 public func run(_ executable: String, _ args: String...) throws {
     try run(executable, args)
 }
 
+/// Run an executable synchronously; uses this process's stdout, stderr, and stdin
+///
+/// - Parameters:
+///   - executable: the executable to run
+///   - args: arguments to pass to the executable
+/// - Throws: RunError if command fails
 public func run(_ executable: String, _ args: [String]) throws {
     let task = Task(executable: executable, args: args)
     let code = task.runSync()
     guard code == 0 else {
-        throw ExecuteError(exitStatus: code)
+        throw RunError(exitStatus: code)
     }
 }
 
+/// Run an executable synchronously and capture its output
+///
+/// - Parameters:
+///   - executable: the executable to run
+///   - args: arguments to pass to the executable
+/// - Returns: the captured data
+/// - Throws: CaptureError if command fails
 public func capture(_ executable: String, _ args: String...) throws -> CaptureResult {
     return try capture(executable, args)
 }
 
+/// Run an executable synchronously and capture its output
+///
+/// - Parameters:
+///   - executable: the executable to run
+///   - args: arguments to pass to the executable
+/// - Returns: the captured data
+/// - Throws: CaptureError if command fails
 public func capture(_ executable: String, _ args: [String]) throws -> CaptureResult {
     let out = PipeStream()
     let err = PipeStream()
@@ -40,10 +66,21 @@ public func capture(_ executable: String, _ args: [String]) throws -> CaptureRes
     return captured
 }
 
+/// Run a bash statement synchronously; uses this process's stdout, stderr, and stdin
+///
+/// - Parameter bash: the bash statement to run
+/// - Throws: RunError if command fails
+/// - Warning: Do not use this with unsanitized user input, can be dangerous
 public func run(bash: String) throws {
     try run("/bin/bash", "-c", bash)
 }
 
+/// Run a bash statement synchronously and capture its output
+///
+/// - Parameter bash: the bash statement to run
+/// - Returns: the captured data
+/// - Throws: CaptureError if command fails
+/// - Warning: Do not use this with unsanitized user input, can be dangerous
 public func capture(bash: String) throws -> CaptureResult {
     return try capture("/bin/bash", "-c", bash)
 }
@@ -52,6 +89,10 @@ public func capture(bash: String) throws -> CaptureResult {
 
 public class Task {
     
+    /// Finds the path to an executable
+    ///
+    /// - Parameter named: the name of the executable to find
+    /// - Returns: the full path to the executable if found, or nil
     public static func findExecutable(named: String) -> String? {
         if named.hasPrefix("/") || named.hasPrefix(".") {
             return named
@@ -61,17 +102,30 @@ public class Task {
     
     private let process: Process
     
+    /// Block to execute when command terminates; default nil
     public var onTermination: ((Int32) -> ())? = nil
+    
+    /// Environment in which to execute the task; defaults to same as this process
     public var env: [String: String] = ProcessInfo.processInfo.environment
+    
+    /// Whether interrupt signals which this process receives should be forwarded to this task; defaults to true
     public var forwardInterrupt = true
     
+    /// The id of the running task
     public var pid: Int32 { return process.processIdentifier }
+    
+    /// Whether task is currently running
     public var isRunning: Bool { return process.isRunning }
     
-    private var stdout: WritableStream?
-    private var stderr: WritableStream?
-    private var stdin: ReadableStream?
-    
+    /// Create a new task
+    ///
+    /// - Parameters:
+    ///   - executable: the executable to run
+    ///   - args: the arguments with which to run the executable; defaults to no arguments
+    ///   - currentDirectory: the directory to run the executable in; defaults to the current process's directory
+    ///   - stdout: the stream which the task should use as it's standard output; defaults to the current process's stdout
+    ///   - stderr: the stream which the task should use as it's standard error; defaults to the current process's stderr
+    ///   - stdin: the stream which the task should use as it's standard input; defaults to the current process's stdin
     public init(executable: String, args: [String] = [], currentDirectory: String? = nil, stdout: WritableStream = WriteStream.stdout, stderr: WritableStream = WriteStream.stderr, stdin: ReadableStream = ReadStream.stdin) {
         self.process = Process()
         self.process.launchPath = Task.findExecutable(named: executable) ?? executable
@@ -82,17 +136,87 @@ public class Task {
         
         if (stdout as? WriteStream) !== WriteStream.stdout {
             self.process.standardOutput = stdout.processObject
-            self.stdout = stdout
         }
         if (stderr as? WriteStream) !== WriteStream.stderr {
             self.process.standardError = stderr.processObject
-            self.stderr = stderr
         }
         if (stdin as? ReadStream) !== ReadStream.stdin {
             self.process.standardInput = stdin.processObject
-            self.stdin = stdin
         }
     }
+    /// Run the task and wait for it to finish
+    ///
+    /// - Returns: the exit code of the completed task
+    @discardableResult
+    public func runSync() -> Int32 {
+        launch()
+        return finish()
+    }
+    
+    /// Run the task but do not wait for it to complete
+    public func runAsync() {
+        launch()
+    }
+    
+    /// Wait for the task to finish; must have already called runAsync
+    ///
+    /// - Returns: the exit code of the completed task
+    @discardableResult
+    public func finish() -> Int32 {
+        process.waitUntilExit()
+        return process.terminationStatus
+    }
+    
+    /// Send the task an interrupt signal
+    public func interrupt() {
+        #if os(Linux)
+        sendSignal(SIGINT)
+        #else
+        process.interrupt()
+        #endif
+    }
+    
+    /// Attempt to suspend the task by sending a stop signal
+    ///
+    /// - Returns: whether it was successful
+    public func suspend() -> Bool {
+        #if os(Linux)
+        return sendSignal(SIGSTOP) == 0
+        #else
+        return process.suspend()
+        #endif
+    }
+    
+    /// Attempt to resume the task by sending a continue signal
+    ///
+    /// - Returns: whether it was successful
+    public func resume() -> Bool {
+        #if os(Linux)
+        return sendSignal(SIGCONT) == 0
+        #else
+        return process.resume()
+        #endif
+    }
+    
+    /// Terminates the task by sending a terminate signal
+    public func terminate() {
+        #if os(Linux)
+        sendSignal(SIGTERM)
+        #else
+        process.terminate()
+        #endif
+    }
+    
+    /// Send a signal to the task
+    ///
+    /// - Parameter sig: the signal to send
+    /// - Returns: result of signal send; 0 means success
+    @discardableResult
+    public func sendSignal(_ sig: Int32) -> Int32 {
+        return kill(pid, sig)
+    }
+    
+    // Helpers
     
     private func launch() {
         if forwardInterrupt {
@@ -111,70 +235,20 @@ public class Task {
         process.launch()
     }
     
-    @discardableResult
-    public func runSync() -> Int32 {
-        launch()
-        return finish()
-    }
-    
-    public func runAsync() {
-        launch()
-    }
-    
-    @discardableResult
-    public func finish() -> Int32 {
-        process.waitUntilExit()
-        return process.terminationStatus
-    }
-    
-    public func interrupt() {
-        #if os(Linux)
-        sendSignal(SIGINT)
-        #else
-        process.interrupt()
-        #endif
-    }
-    
-    public func suspend() -> Bool {
-        #if os(Linux)
-        return sendSignal(SIGSTOP) == 0
-        #else
-        return process.suspend()
-        #endif
-    }
-    
-    public func resume() -> Bool {
-        #if os(Linux)
-        return sendSignal(SIGCONT) == 0
-        #else
-        return process.resume()
-        #endif
-    }
-    
-    public func terminate() {
-        #if os(Linux)
-        sendSignal(SIGTERM)
-        #else
-        process.terminate()
-        #endif
-    }
-    
-    @discardableResult
-    public func sendSignal(_ sig: Int32) -> Int32 {
-        return kill(pid, sig)
-    }
-    
 }
 
 // MARK: -
 
-public struct ExecuteError: ProcessError {
+public struct RunError: ProcessError {
+    /// The status which the task exited with
     public let exitStatus: Int32
     public let message: String? = nil
 }
 
 public struct CaptureError: ProcessError {
+    /// The status which the task exited with
     public let exitStatus: Int32
+    /// Data which was captured prior to the process failing
     public let captured: CaptureResult
     
     public var message: String? {
@@ -183,12 +257,18 @@ public struct CaptureError: ProcessError {
 }
 
 public struct CaptureResult {
+    /// The full stdout contents; use `stdout` for trimmed contents
     public let rawStdout: String
+    
+    /// The full stderr contents; use `stdout` for trimmed contents
     public let rawStderr: String
     
+    /// The stdout contents, trimmed of whitespace
     public var stdout: String {
         return rawStdout.trimmingCharacters(in: .whitespacesAndNewlines)
     }
+    
+    /// The stderr contents, trimmed of whitespace
     public var stderr: String {
         return rawStderr.trimmingCharacters(in: .whitespacesAndNewlines)
     }
