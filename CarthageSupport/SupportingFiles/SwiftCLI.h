8710381cd4b3224d9d8c63283508df16c0b13f47
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-11-05 18:06:16 +0100

    Add .xcodeproj file with shared scheme to support Carthage

---
+++ CarthageSupport/SupportingFiles/SwiftCLI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCLI.h
+//  SwiftCLI
+//
+//  Created by Cihat Gündüz on 05.11.18.
+//  Copyright © 2018 Jake Heiser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCLI.
+FOUNDATION_EXPORT double SwiftCLIVersionNumber;
+
+//! Project version string for SwiftCLI.
+FOUNDATION_EXPORT const unsigned char SwiftCLIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCLI/PublicHeader.h>
+
+
